301ef5733f0571d3db2216053b86b94d5dc0ec0b
Kamran Payne <mail@kamranpayne.com>
2015-06-06 22:51:49 -0400

    recompile

--- www/js/auth.js
+++ www/js/auth.js
@@ -1,51 +1,74 @@
 //auth.js
 // user authentication logic goes here
 
-var isNewUser = true;
-
-var authRef = new Firebase("https://typy.firebaseio.com");
-var authData = authRef.getAuth();
-
-if (authData) {
-    console.log("User " + authData.uid + " is logged in with " + authData.provider);
-} else {
-    console.log("User is logged out");
-}
-
-
-
-// callback function
-function authHandler(error, authData) {
-    if (error) {
-        console.log("Login Failed!", error);
-    } else {
-        console.log("Authenticated successfully with payload:", authData);
-
-        window.location.href = "/";
+app.factory("Auth", ["$firebaseAuth",
+    function($firebaseAuth) {
+        var ref = new Firebase("https://typy.firebaseio.com");
+        return $firebaseAuth(ref);
     }
-}
+]);
+
+app.controller("authCtrl", ["$scope", "Auth",
+    function($scope, Auth) {
+
+        $scope.authData = null;
+
+        $scope.login = function() {
+            
+            $scope.error = null;
+
+            Auth.$authWithPassword({
+                email: $scope.email,
+                password: $scope.password
+            }).then(function(authData) {
+                console.log("Logged in as: ", authData.uid);
+                window.location.replace("/");
+            }).catch(function(error) {
+                console.error("Authentication failed: ", error);
+            });
+        };
 
 
-//login authentication
-function authenticatePassword(userEmail, userPassword) {
-    authRef.authWithPassword({
-        email    : $('#login-email').val(),
-        password : $('#login-password').val()
-    }, authHandler);
+        $scope.createUser = function() {
+            $scope.error = null;
+
+            Auth.$createUser({
+                username: $scope.username,
+                email: $scope.email,
+                password: $scope.password
+            }).then(function(authData) {
+                $scope.message = "User created with uid: " + authData.uid;
+            }).catch(function(error) {
+                $scope.error = error;
+            });
+        };
+
+        // $scope.removeUser = function() {
+        //     $scope.message = null;
+        //     $scope.error = null;
+
+        //     Auth.$removeUser({
+        //         email: $scope.email,
+        //         passsword: $scope.password
+        //     }).then(function() {
+        //         $scope.message = "User removed";
+        //     }).catch(function(error) {
+        //         $scope.error = error;
+        //     });
+        // };
 
 
-}
+        // if ($scope.authData) {
+        //     console.log("User " + authData.uid + " is logged in with " + authData.provider);
+        // } else {
+        //     console.log("User is logged out");
+        // }
 
+        $scope.logout = function() {
+            Auth.$unauth();
+            window.location.replace("/");
+        };
 
-
-// authRef.onAuth(function(authData) {
-//     // if (authData && isNewUser) {
-//     //     authRef.child("users").child(authData.uid).set({
-//     //         provider: authData.provider,
-//     //         name: getName(authData)
-//     //     });
-//     // }
-
-
-// });
-
+        authData = Auth.$getAuth();
+    }
+]);
