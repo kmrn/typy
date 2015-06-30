e287f627dd31120eff8d016288b52a8dcbbf06fc
Kamran Payne <mail@kamranpayne.com>
2015-06-30 01:40:30 -0400

    recompile

--- www/js/auth.js
+++ www/js/auth.js
@@ -21,8 +21,8 @@ app.factory("Profile", ["$firebaseObject",
     }
 ]);
 
-app.controller("authCtrl", ["$scope", "Auth", "Profile",
-    function($scope, Auth, Profile) {
+app.controller("authCtrl", ["$scope", "$timeout", "Auth", "Profile",
+    function($scope, $timeout, Auth, Profile) {
 
         $scope.auth = Auth;
 
@@ -106,6 +106,53 @@ app.controller("authCtrl", ["$scope", "Auth", "Profile",
             });
         };
 
+        $scope.changeDisplayName = function() {
+            $scope.user.displayName = $scope.displayName;
+            $scope.user.$save().then(function() {
+                $scope.displayNameChange.$setPristine();
+                $scope.displayName = null;
+
+                var element = angular.element( document.querySelector('.displayname-success') );
+                element.removeClass('hidden');
+                $timeout(function() {
+                    element.addClass('hidden');
+                }, 3000);
+                console.log("name changed");
+            });
+        }
+
+        $scope.resetPassword = function() {
+            Auth.$resetPassword({
+                email: $scope.email
+            }).then(function() {
+                console.log("Password reset email sent successfully!");
+            }).catch(function(error) {
+                var element = angular.element( document.querySelector('.invalid') );
+                element.removeClass("hidden");
+                console.error("Error: ", error);
+            });
+        };
+
+        $scope.changePassword = function() {
+            Auth.$changePassword({
+                email: authData.password.email,
+                oldPassword: $scope.oldPassword,
+                newPassword: $scope.newPassword
+            }).then(function() {
+                $scope.passwordChange.$setPristine();
+                $scope.oldPassword = null;
+                $scope.newPassword = null;
+
+                var element = angular.element( document.querySelector('.change-password-success') );
+                element.removeClass('hidden');
+                $timeout(function() {
+                    element.addClass('hidden');
+                }, 3000);
+                console.log("Password changed successfully!");
+            }).catch(function(error) {
+                console.error("Error: ", error);
+            });
+        }
 
         $scope.logout = function() {
             Auth.$unauth();
@@ -121,7 +168,7 @@ app.controller("authCtrl", ["$scope", "Auth", "Profile",
             } else {
                 return false;
             }
-        }
+        };
 
         //user = Profile(authData.uid);
 
