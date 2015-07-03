b51dd47d51d99efd6c85543fc25d19ed5dfd1296
Kamran Payne <mail@kamranpayne.com>
2015-07-03 00:08:21 -0400

    update adblock text

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,7 @@ window.onload = function() {
         var ad = document.querySelector("ins.adsbygoogle");
         if (ad && ad.innerHTML.replace(/\s/g, "").length == 0) {
             ad.style.cssText = 'display:block !important; text-decoration: none';
-            ad.innerHTML = "<h5>Know what would be awesome?</h5><p>If you disabled adblock. If you don't want to support us that way, at least contribute to our patreon for a guilt free Typy experience.</p>";
+            ad.innerHTML = "<h5>Ads</h5><p>aren't that great, not gonna lie. But Typy isn't free to host and took a lot of effort to make. We'd appreciate it if you turned off adblock. Thanks!</p><p>- Kamran</p>";
         }
     }); 
 }; 
