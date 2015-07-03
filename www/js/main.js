1963548297cdc9db447570b6d951709cb6f36152
Kamran Payne <mail@kamranpayne.com>
2015-07-03 00:00:04 -0400

    recompile

--- www/js/main.js
+++ www/js/main.js
@@ -8,7 +8,7 @@ window.onload = function() {
         var ad = document.querySelector("ins.adsbygoogle");
         if (ad && ad.innerHTML.replace(/\s/g, "").length == 0) {
             ad.style.cssText = 'display:block !important; text-decoration: none';
-            ad.innerHTML = "<h5>Did you know you can disable ads and still support Typy by buying <u>Typy Gold</u>?</h5> It's true! Every Typy Gold subscriber gets no ads and the warm fuzzy feeling of supporting content you use. I do this for a living man, come on. <hr><a href='/gold.html' class='button button-primary'>Get Gold</a>";
+            ad.innerHTML = "<h5>Know what would be awesome?</h5><p>If you disabled adblock. If you don't want to support us that way, at least contribute to our patreon for a guilt free Typy experience.</p>";
         }
     }); 
 }; 
